72af6e997601ec3cd988c785600eeb5e3d9b5445
Mielein <marie.trojan@uni-weimar.de>
2023-04-15 18:27:21 +0200

    scooter images

--- Assets/Scripts/ShopCanvas.cs
+++ Assets/Scripts/ShopCanvas.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShopCanvas : MonoBehaviour {
 
     public GameObject[] vehicle_arr;
     public GameObject[] diff_arr;
+    public Button left_button;
+    public Button right_button;
+    public Sprite burntnuggie_l;
+    public Sprite burntnuggie_r;
     private int i = 0;
 
     void Start(){
-       
 
     }
 
@@ -39,5 +43,9 @@ public class ShopCanvas : MonoBehaviour {
             i=4;
         }
         vehicle_arr[i].SetActive(true);
+    }
+
+    public void changeLeftButton(){
+        left_button.GetComponent<Image>().sprite = burntnuggie_l;
     }
 }
