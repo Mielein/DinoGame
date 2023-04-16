532f9ee670c515d655ffd24e92f7ab4f58549bac
Mielein <marie.trojan@uni-weimar.de>
2023-04-16 20:01:30 +0200

    lame

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,6 +10,18 @@ public class GameManager : MonoBehaviour {
     private bool paused = false;
     private float _movement = 1.0f;
     private float currmov = 0.0f;
+    public AudioSource audio;
+
+    public bool skateboard;
+    public bool scooter;
+    public bool car;
+    public bool plane;
+
+    public GameObject skatelock;
+    public GameObject scootlock;
+    public GameObject carlock;
+    public GameObject planelock;
+
 
     public GameObject pause_menu;
 
@@ -28,23 +40,65 @@ public class GameManager : MonoBehaviour {
     }
 
     void Update(){
+        
         Debug.Log(paused);
         if(SceneManager.GetActiveScene().name != "Game"){
             Pause();
         }
+
+        NuggetCount();
+    }
+
+    public void NuggetCount(){
+        if(_nuggets >= 100){
+            skateboard = true;
+
+        }
+        if(_nuggets >= 200){
+            scooter = true;
+        }
+        if(_nuggets >= 300){
+            car = true;
+        }
+        if(_nuggets >= 400){
+            plane = true;
+        }
+    }
+
+    public void Unlock(){
+        if(skateboard){
+            skatelock.SetActive(false);
+            SetMovement(-0.1f);
+        }
+        if(scooter){
+            scootlock.SetActive(false);
+            SetMovement(-0.2f);
+        }
+        if(car){
+            carlock.SetActive(false);
+            SetMovement(-0.2f);
+        }
+        if(plane){
+            planelock.SetActive(false);
+            SetMovement(-0.2f);
+        }
     }
 
     private void Pause(){
+        
         if(Input.GetKeyDown(KeyCode.Escape)){
             pause_menu.SetActive(true);
             Time.timeScale = 0.0f;
+            audio.volume = 0.3f;
         }
         
     }
 
+
     public void EndPause(){
         pause_menu.SetActive(false);
         Time.timeScale = 1.0f;
+        audio.volume = 0.5f;
         
     }
 
