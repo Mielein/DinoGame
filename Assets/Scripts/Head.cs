3e9fe2b19839be14e83b00075ae6e330480bffbf
jreeee <jeremias.michael.kilian@uni-weimar.de>
2023-04-16 19:48:16 +0200

    looping bg, nuggies, parralax reversed

--- Assets/Scripts/Head.cs
+++ Assets/Scripts/Head.cs
@@ -9,7 +9,8 @@ public class Head : MonoBehaviour
     public int count;
     private int multiplier;
     GameObject go;
-    Dino player = null;    
+    Dino player = null;
+    int[] reps = new int[] {0, 0, 0, 0};
 
     public GameObject road;
     public GameObject front;
@@ -35,19 +36,20 @@ public class Head : MonoBehaviour
     }
     void OnTriggerEnter2D(Collider2D other) {
         float movement = GameManager.Instance.GetMovement();
-        Debug.Log("idk");
+        multiplier = (int) (-200.0f * movement);
+        Debug.Log(multiplier);
         bool hide = true;
         if (other.gameObject.tag == "good") {
-            count += (int) (3.0f * movement);
+            count += (int) (3.0f * multiplier);
         }
         else if (other.gameObject.tag == "raw") {
-            count += (int) (1.0f * movement);
+            count += (int) (1.0f * multiplier);
         }
         else if (other.gameObject.tag == "bad") {
-            count += (int) (5.0f * movement);
+            count += (int) (5.0f * multiplier);
         }
         if (other.gameObject.tag == "obs-1") {
-            count -= (int) (1.0f * movement);
+            count -= (int) (1.0f * multiplier);
         }
         else if (other.gameObject.tag == "obs-2") {
             count += (int) (2.0f * multiplier);
@@ -57,34 +59,41 @@ public class Head : MonoBehaviour
         }
         else if (other.gameObject.tag == "Road") {
             hide = false;
-            Instantiate(road);
+            GameObject tmp = Instantiate(road);
+            reps[0] += 1;
+            tmp.transform.position = new Vector3(roadspawn.transform.position.x + (100.0f * reps[0]), roadspawn.transform.position.y, roadspawn.transform.position.z);
             Destroy(other);
             Debug.Log("load road");
         }
         else if (other.gameObject.tag == "Front") {
             hide = false;
             GameObject tmp = Instantiate(front);
-            tmp.transform.position = frontspawn.transform.position/* new Vector3(frontspawn.transform.position.x + 60.0f, frontspawn.transform.position.y, frontspawn.transform.position.z) */;
-            //Destroy(other);
-            Debug.Log("load front");
+            reps[1] += 1;
+            tmp.transform.position = new Vector3(frontspawn.transform.position.x + (58.0f * reps[1]), frontspawn.transform.position.y, frontspawn.transform.position.z);
+            Destroy(other);
+            Debug.Log("load front, " + tmp.transform.position);
             // move sprite
         }
         else if (other.gameObject.tag == "Middle") {
             hide = false;
-            Instantiate(middle);
+            GameObject tmp = Instantiate(middle);
+            reps[2] += 1;
+            tmp.transform.position = new Vector3(middlespawn.transform.position.x + (58.0f * reps[2]), middlespawn.transform.position.y, middlespawn.transform.position.z);
             Destroy(other);
             Debug.Log("load middle");
             // move sprite
         }
         else if (other.gameObject.tag == "Back") {
             hide = false;
-            Instantiate(back);
+            GameObject tmp = Instantiate(back);
+            reps[3] += 1;
+            tmp.transform.position = new Vector3(backspawn.transform.position.x + (58.0f * reps[3]), backspawn.transform.position.y, backspawn.transform.position.z);
             Destroy(other);
             Debug.Log("load back");
             // move sprite
         }
-        points.text = "Nuggies: " + count;
         if (hide) {
+            points.text = "Nuggies: " + count;
             other.GetComponent<SpriteRenderer>().enabled = false;
         }
     }
